dc9d0363cb7c88d2c3a6e165bad6b529b5d4f8c4
Jay Moore <jay@Jays-MacBook-Pro.local>
2015-01-30 17:52:19 +0000

    Template-Page
    Started creating the template page design just Div’s and colours at the
    moment

---
+++ template-page.php
@@ -0,0 +1,29 @@
+<html>
+<link href="CSSDesign.css" rel="stylesheet" type="text/css" />
+<body>
+<div class="top-bar">
+<div class="Logo">
+    
+</div> 
+<div class="login">
+
+    
+    
+      
+</div>
+<div class="Search_Bar">
+    
+</div>
+</div>
+    
+    
+    
+  </body>     
+<footer>
+    
+    
+    </footer>    
+
+
+
+</html>

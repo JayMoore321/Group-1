48309f66c60ce061b4f6c5352e8534aef3777e9a
Jay Moore <jay@Jays-MacBook-Pro.local>
2015-02-02 16:48:05 +0000

    CSS Test
    Not much changed issues to bring up on tuesdays meeting about alignment
    logo added.

--- template-page.php
+++ template-page.php
@@ -3,17 +3,21 @@
 <body>
 <div class="Top-Bar">
 <div class="Logo">
+<img src="LOGO2.jpg.png" alt="lOGO" style="width:200px; height:150px">
+</div>
+<div class="Hold">
+<!--<div class="Login">
+  
+</div>
+<div class="Search_Bar">
+</div>
+<div class="Basket">
     
-</div> 
-<div class="Login">
-
-    
-    
-      
+</div>
 </div>
 <div class="Nav_Bar">
     
-</div>
+</div> -->
 </div>
     
     

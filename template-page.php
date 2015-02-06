7049efc754270a4e649e08a05bfaede16ab8c19e
HarleyDobson <harley94@btinternet.com>
2015-02-06 10:13:57 +0000

    Update template-page.php

--- template-page.php
+++ template-page.php
@@ -1,5 +1,26 @@
 <html>
+<head>
 <link href="CSSDesign1.css" rel="stylesheet" type="text/css" />
+<link href="navbar.css" rel="stylesheet" type="text/css" />
+
+<script type="text/javascript">
+
+var image = new Array()
+image[0] = new Image()
+image[0].src = "slideshow1.jpg"
+image[1] = new Image()
+image[1].src = "slideshow2.jpg"
+image[2] = new Image()
+image[2].src = "slideshow3.jpg"
+image[3] = new Image()
+image[3].src = "slideshow4.jpg"
+
+</script>
+
+<Title> Prism Games</title>
+
+</head>
+
 <body>
 <div class="Pagewrap">
 <div class="Top-Bar">
@@ -29,9 +50,27 @@
 
     
 </div>
-</div>
-    
-    
+<div>
+<img src="slideshow1.jpg" id="slide" width="1050" height="400" />
+
+<script type="text/javascript">
+
+
+var show=0
+
+function slider(){
+ document.getElementById('slide').src = image[show].src
+ if (show<3)
+  show++
+ else
+  show=0
+
+ setTimeout("slider()",3000)
+}
+
+slider()
+</script>    
+</div>   
     
   </body>     
 <footer>

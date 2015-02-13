a421670edae4e65204cf5ad1f1d836960c3e695b
HarleyDobson <harley94@btinternet.com>
2015-02-13 10:37:04 +0000

    Create destiny.php

---
+++ destiny.php
@@ -0,0 +1,132 @@
+<html>
+<head>
+<link href="CSSDesign1.css" rel="stylesheet" type="text/css" />
+<link href="navbar.css" rel="stylesheet" type="text/css" />
+
+
+<style type="text/css">
+
+div.footer
+{
+padding:1 em;
+color:white;
+background-color:teal;
+clear:left;
+font-size:70%;
+text-align:center;
+}
+
+</style>
+
+<?php session_start();?>
+
+<Title> Prism Games | Destiny</title>
+
+</head>
+
+<body>
+<div class="Pagewrap">
+<div class="Top-Bar">
+<div class="Logo">
+ 
+<a href="Homepage.php">
+  <img src="LOGO2.jpg.png" alt="lOGO" style="width:200px; height:150px">
+</a> 
+
+</div>
+<div class="Hold">
+<div class="Login">
+<?php include('login-script.php'); ?>
+</div>
+	<div class="Search_Bar">
+<script>
+  (function() {
+    var cx = '005131504979053412877:yw_sh9c9oda';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+        '//www.google.com/cse/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+  })();
+</script>
+<gcse:search></gcse:search>
+	</div>
+	<div class="Basket">
+
+    </div>
+</div>
+<div id='nav'>
+<ul>
+   <li class='active'><a href='Homepage.php'><span>Home</span></a></li>
+   <li><a href='ps4page.php'><span>Ps4</span></a></li>
+   <li><a href='xboxonepage.php'><span>Xbox One</span></a></li>
+   <li><a href='latestoffers.php'><span>Latest Offers</span></a></li>
+   <li class='last'><a href='newreleases.php'><span>New Releases</span></a></li>
+</ul>
+
+ 
+
+</div>
+
+</br>
+<h1>Destiny Day One Edition</h1>
+
+ 
+     
+<div  style="float:left">
+
+
+
+<img src ="./gameimages/001.png" width="300px" height="450px">
+
+
+ 
+
+</div
+
+<div>
+</div>
+
+
+
+<div  style="float:left">
+</br>
+<ul>
+   	<li>Destiny is an online-only title and requires access to the PlayStation Network. </li>
+	<li>From the creators of Halo and the publisher of Call of Duty, comes Destiny on PlayStation 4 </li>
+	<li>As a Guardian of the last safe city on earth you are tasked with defending the City. </li>
+	<li>Create your character, customise your weaponry and forge your own Destiny. </li>
+   	<li>Play with friends in competitive and cooperative modes </li>
+
+</ul>
+
+</br>
+
+<p><h3>Price: £39.99</h3></p>
+
+<a href="destiny.php">
+  <img src="basket.png" alt="lOGO" style="width:100px; height:30px">
+</a> 
+
+
+	</div>
+
+ 
+
+
+    
+    
+<div class="footer">
+<script> 
+document.write(document.lastModified);
+</script>
+<li><a href="feedbackform.php">Feedback Form</a></li>
+	<li><a href="">Contact Us</a></li>
+	<li><a href="">Product Guarantee </a></li>
+</div>
+        
+    
+</body>
+</html>

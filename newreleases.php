e9f8b95648ebcad1fba66fa9b4bcf3e648e3e24d
HarleyDobson <harley94@btinternet.com>
2015-02-20 10:07:59 +0000

    Create newreleases.php

---
+++ newreleases.php
@@ -0,0 +1,245 @@
+<html>
+<head>
+<link href="CSSDesign1.css" rel="stylesheet" type="text/css" />
+<link href="navbar.css" rel="stylesheet" type="text/css" />
+<?php session_start();?>
+
+<Title> Prism Games | New Releases</title>
+</head>
+<body>
+<div class="Pagewrap"><div class="Top-Bar"><div class="Logo"><!--
+--><img src="LOGO3.png" alt="lOGO" style="width:200px; height:150px"><h1 style="font-size: 14px; text-align: center;">PRISM GAMES</h1><!--
+--></div><div class="Hold"><div class="Login">
+<?php include('login-script.php'); ?>
+</div>
+<div class="Search_Bar">
+<script>
+  (function() {
+    var cx = '005131504979053412877:yw_sh9c9oda';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+        '//www.google.com/cse/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+  })();
+</script>
+<gcse:search></gcse:search>
+	</div>
+	<div class="Basket">
+
+
+	
+    </div></div>
+<div id='nav'>
+<ul>
+   <li class='active'><a href='Homepage.php'><span>Home</span></a></li>
+   <li><a href='ps4page.php'><span>Ps4</span></a></li>
+   <li><a href='xboxonepage.php'><span>Xbox One</span></a></li>
+   <li><a href='latestoffers.php'><span>Latest Offers</span></a></li>
+   <li class='last'><a href='newreleases.php'><span>New Releases</span></a></li>
+</ul>
+
+</div>
+</div>
+<div class="Main-Content">
+</div>
+
+<div class="promo">
+<div style ="width: 280px; margin:10 auto; margin-right:10; height:400px; border:30px solid purple; border-radius:30px; -moz-border-radius:30px; -webkit-border-radius:30px; display: inline-block;" >
+ <table style="background:green; border="0" width="280px" align="center" border-spacing: 10px; display: inline-block; ">
+
+
+<tr>  
+<tr width="200px">
+    <td align="center"><h2> Destiny</h2> </td>
+
+
+  </tr>
+
+<tr width="200px">
+    <td align="center">  <img src ="./gameimages/001.png" width="200px" height="300px"> </td>
+
+  </tr>
+  <tr>
+    <td align="center"><FORM METHOD="LINK" ACTION="">
+<INPUT TYPE="submit" VALUE="Buy Here">
+</FORM></td>		
+
+  </tr>
+
+
+  
+</table> 
+ 
+ 
+</div>  
+
+<div style ="width: 280px; margin:10 auto; margin-right:10; height:400px; border:30px solid purple; border-radius:30px; -moz-border-radius:30px; -webkit-border-radius:30px; display: inline-block;" >
+ <table style="background:green; border="0" width="280px" align="center" border-spacing: 10px;">
+
+
+<tr>  
+<tr width="200px">
+    <td align="center"><h2> Just Dance 2015</h2> </td>
+
+
+  </tr>
+
+<tr width="200px">
+    <td align="center">  <img src ="./gameimages/024.png" width="200px" height="300px"> </td>
+
+  </tr>
+  <tr>
+    <td align="center"><FORM METHOD="LINK" ACTION="">
+<INPUT TYPE="submit" VALUE="More Info">
+</FORM></td>		
+
+  </tr>
+
+
+  
+</table> 
+ 
+ 
+</div>  
+
+
+<div style ="width: 282px; margin:10 auto; height:400px; border:30px solid purple; border-radius:30px; -moz-border-radius:30px; -webkit-border-radius:30px; display: inline-block;" >
+ <table style="background:green; border="0" width="282px" align="center" border-spacing: 10px;">
+
+
+<tr>  
+<tr width="200px">
+    <td align="center"><h2> Alien Isolation</h2> </td>
+
+
+  </tr>
+
+<tr width="200px">
+    <td align="center">  <img src ="./gameimages/057.png" width="200px" height="300px"></td>
+
+  </tr>
+  <tr>
+    <td align="center"><FORM METHOD="LINK" ACTION="">
+<INPUT TYPE="submit" VALUE="More Info">
+</FORM></td>		
+
+  </tr>
+
+
+  
+</table> 
+ 
+ 
+
+</div>  
+
+<div style ="width: 280px;  height:400px; border:30px solid purple; border-radius:30px; -moz-border-radius:30px; -webkit-border-radius:30px; display: inline-block;" >
+ <table style="background:green; border="0" width="280px" align="center" border-spacing: 10px;">
+
+
+<tr>  
+<tr width="200px">
+    <td align="center"><h2> Diablo</h2> </td>
+
+
+  </tr>
+
+<tr width="200px">
+    <td align="center">  <img src ="./gameimages/039.png" width="200px" height="300px"></td>
+
+  </tr>
+  <tr>
+    <td align="center"><FORM METHOD="LINK" ACTION="">
+<INPUT TYPE="submit" VALUE="More Info">
+</FORM></td>		
+
+  </tr>
+
+
+  
+</table> 
+ 
+ 
+
+</div>  
+
+<div style ="width: 280px; margin-right: 10; margin-left: 10; height:400px; border:30px solid purple; border-radius:30px; -moz-border-radius:30px; -webkit-border-radius:30px; display: inline-block;" >
+ <table style="background:green; border="0" width="280px" align="center" border-spacing: 10px;">
+
+
+<tr>  
+<tr width="200px">
+    <td align="center"><h2> UFC</h2> </td>
+
+
+  </tr>
+
+<tr width="200px">
+    <td align="center">  <img src ="./gameimages/070.png" width="200px" height="300px"></td>
+
+  </tr>
+  <tr>
+    <td align="center"><FORM METHOD="LINK" ACTION="">
+<INPUT TYPE="submit" VALUE="More Info">
+</FORM></td>		
+
+  </tr>
+
+
+  
+</table> 
+ 
+ 
+
+</div>  
+
+
+<div style ="width: 282px;  height:400px; border:30px solid purple; border-radius:30px; -moz-border-radius:30px; -webkit-border-radius:30px; display: inline-block;" >
+ <table style="background:green; border="0" width="282px" align="center" border-spacing: 10px;">
+
+
+<tr>  
+<tr width="200px">
+    <td align="center"><h2> Minecraft</h2> </td>
+
+
+  </tr>
+
+<tr width="200px">
+    <td align="center">  <img src ="./gameimages/008.png" width="200px" height="300px"></td>
+
+  </tr>
+  <tr>
+    <td align="center"><FORM METHOD="LINK" ACTION="">
+<INPUT TYPE="submit" VALUE="More Info">
+</FORM></td>		
+
+  </tr>
+
+
+  
+</table> 
+ 
+
+</div>
+    
+</div>    
+<div class="footer">
+
+<script> 
+document.write(document.lastModified);
+</script>
+<li><a href="feedbackform.php">Feedback Form</a></li>
+	<li><a href="">Contact Us</a></li>
+	<li><a href="">Product Guarantee </a></li>
+
+	
+</div>
+        
+
+ </div>   
+</body>
+</html>

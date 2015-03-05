59f47f0b54024b54aeaf0ec2dcf6d468c0fc02cb
HarleyDobson <harley94@btinternet.com>
2015-03-05 18:00:20 +0000

    Create under40.php

---
+++ under40.php
@@ -0,0 +1,228 @@
+<html>
+<head>
+<link href="CSSDesign1.css" rel="stylesheet" type="text/css" />
+<link href="navbar.css" rel="stylesheet" type="text/css" />
+
+
+<link rel='shortcut icon' type='image/ico' href='favicon.ico' />
+
+
+
+<?php session_start();?>
+
+
+
+
+<Title> Prism Games | Under £40</title>
+
+</head>
+
+<body>
+<div class="Pagewrap"><div class="Top-Bar"><div class="Logo"><!--
+--><img src="LOGO3.png" alt="lOGO" style="width:200px; height:150px"><h1 style="font-size: 14px; text-align: center;">PRISM GAMES</h1><!--
+--></div><div class="Hold"><div class="Login">
+<?php include('login-script.php'); ?>
+</div>
+<div class="Search_Bar">
+<script>
+  (function() {
+    var cx = '005131504979053412877:yw_sh9c9oda';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+        '//www.google.com/cse/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+  })();
+</script>
+<gcse:search></gcse:search>
+	</div>
+	<div class="Basket">
+
+        <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post" >
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIG1QYJKoZIhvcNAQcEoIIGxjCCBsICAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYC3jjQhaGU3b83dmmcbLQPDYnV8u24jwJ2lOAEXGOGCpQSTIbmtr1scrnC3Ahj7qFYfSyQEse/ZGAHOImb9+2U9NaoJUsDp7KIL+R2JbJL5VGsFAj4lrh/gkXRw59TtokEwFdwWhYAvkpTzm+TuDPCf74lCIbgDHKxk07rI9cJydzELMAkGBSsOAwIaBQAwUwYJKoZIhvcNAQcBMBQGCCqGSIb3DQMHBAg/yIDp6UPu14AwrpmXsneMxaj2Crjoc3WlrRDKFgnDuuqHNpIxuRT0IAiBNyOfo49g7z2DrQT8lDkQoIIDhzCCA4MwggLsoAMCAQICAQAwDQYJKoZIhvcNAQEFBQAwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tMB4XDTA0MDIxMzEwMTMxNVoXDTM1MDIxMzEwMTMxNVowgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDBR07d/ETMS1ycjtkpkvjXZe9k+6CieLuLsPumsJ7QC1odNz3sJiCbs2wC0nLE0uLGaEtXynIgRqIddYCHx88pb5HTXv4SZeuv0Rqq4+axW9PLAAATU8w04qqjaSXgbGLP3NmohqM6bV9kZZwZLR/klDaQGo1u9uDb9lr4Yn+rBQIDAQABo4HuMIHrMB0GA1UdDgQWBBSWn3y7xm8XvVk/UtcKG+wQ1mSUazCBuwYDVR0jBIGzMIGwgBSWn3y7xm8XvVk/UtcKG+wQ1mSUa6GBlKSBkTCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb22CAQAwDAYDVR0TBAUwAwEB/zANBgkqhkiG9w0BAQUFAAOBgQCBXzpWmoBa5e9fo6ujionW1hUhPkOBakTr3YCDjbYfvJEiv/2P+IobhOGJr85+XHhN0v4gUkEDI8r2/rNk1m0GA8HKddvTjyGw/XqXa+LSTlDYkqI8OwR8GEYj4efEtcRpRYBxV8KxAW93YDWzFGvruKnnLbDAF6VR5w/cCMn5hzGCAZowggGWAgEBMIGUMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbQIBADAJBgUrDgMCGgUAoF0wGAYJKoZIhvcNAQkDMQsGCSqGSIb3DQEHATAcBgkqhkiG9w0BCQUxDxcNMTUwMjE5MTczMDA0WjAjBgkqhkiG9w0BCQQxFgQUV/n8SEbkbxc11B3xD5In7Za6NmcwDQYJKoZIhvcNAQEBBQAEgYCFdMdo9TTEjN1qc45PDzH7BHckskrHNwL5byIS4dm9w5DmNQIdYY/v9/BluziMlthhzDPWbn7TGGDr4BM4ykYAUQMwMwiTv3222EH+xLwyFkM0F9ft6078gZUjfsYfoKmybmV1A8AXbIkCFR1GSn77MlF2DpGNTS8WrjEORhXmuw==-----END PKCS7-----
+">
+<input type="image" src="http://www.computing.northampton.ac.uk/~2027c_1/Cart.png" border="0" name="submit" alt="PayPal – The safer, easier way to pay online." wisth="50" height="50">
+<img alt="" border="0" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" width="1" height="1">
+</form>
+    </div>
+</div>
+<div id='nav'>
+<ul>
+   <li class='active'><a href='Homepage.php'><span>Home</span></a></li>
+   <li><a href='ps4page.php'><span>Ps4</span></a></li>
+   <li><a href='xboxonepage.php'><span>Xbox One</span></a></li>
+   <li><a href='latestoffers.php'><span>Latest Offers</span></a></li>
+   <li class='last'><a href='newreleases.php'><span>New Releases</span></a></li>
+</ul>
+ 
+</div>
+</div>
+<div class="Main-Content">
+
+
+    
+ <div style ="width: 1050px; margin:0 auto; height:1000px; border:1px solid black; overflow:scroll; overflow-y:hidden;"">
+ <table class="ps4table" border="0" width="2000px" align="center" "border-spacing: 10px;">
+  <tr width="200px">
+
+    <td style="padding:20px 20px 20px 20px;"><img src ="./gameimages/3.png" width="200px" height="300px"> </td>
+
+    <td style="padding:20px 20px 20px 20px;"><img src ="./gameimages/5.png" width="200px" height="300px"> </td>
+
+    <td style="padding:20px 20px 20px 20px;"><img src ="./gameimages/11.png" width="200px" height="300px"> </td>
+
+    <td style="padding:20px 20px 20px 20px;"><img src ="./gameimages/13.png" width="200px" height="300px"> </td>
+    <td style="padding:20px 20px 20px 20px;"><img src ="./gameimages/16.png" width="200px" height="300px"> </td>
+
+    <td style="padding:20px 20px 20px 20px;"><img src ="./gameimages/19.png" width="200px" height="300px"> </td>
+    
+    <td style="padding:20px 20px 20px 20px;"><img src ="./gameimages/43.png" width="200px" height="300px"> </td>
+
+    <td style="padding:20px 20px 20px 20px;"><img src ="./gameimages/45.png" width="200px" height="300px"> </td>
+
+    <td style="padding:20px 20px 20px 20px;"><img src ="./gameimages/49.png" width="200px" height="300px"> </td>
+    <td style="padding:20px 20px 20px 20px;"><img src ="./gameimages/50.png" width="200px" height="300px"> </td>
+
+	<td style="padding:20px 20px 20px 20px;"><img src ="./gameimages/52.png" width="200px" height="300px"> </td>
+
+    
+  </tr>
+
+<div id='ps4page'>
+
+</div>
+
+<div id='ps4page'>
+
+</div>
+  <tr>
+
+	<td style="padding:0 20px 20px 20px;">Call Of Duty Advanced Warfare<br>Price: £32.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=3">More Info</td></h3>
+
+	<td style="padding:0 20px 20px 20px;">Saints Row IV<br>Price: £34.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=5">More Info</td></h3>
+
+
+	<td style="padding:0 20px 20px 20px;">Driveclub<br>Price: £30.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=11">More Info</td></h3>
+
+
+	<td style="padding:0 20px 20px 20px;">Shadow Of Mordor<br>Price: £32.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=13">More Info</td></h3>
+
+
+	<td style="padding:0 20px 20px 20px;">WWE 2K15<br>Price: £32.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=16">More Info</td></h3>
+
+
+	<td style="padding:0 20px 20px 20px;">The Last Of Us<br>Price: £34.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=19">More Info</td></h3>
+
+         <td style="padding:0 20px 20px 20px;">Saints Row IV<br>Price: £30.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=43">More Info</td></h3>
+
+
+
+	<td style="padding:0 20px 20px 20px;">Fifa 15<br>Price: £32.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=45">More Info</td></h3>
+
+
+
+	<td style="padding:0 20px 20px 20px;">Forza Horison 2<br>Price: £34.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=49">More Info</td></h3>
+
+	<td style="padding:0 20px 20px 20px;">The Crew<br>Price: £32.00 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=50">More Info</td></h3>
+
+
+
+	<td style="padding:0 20px 20px 20px;">Rugby 15<br>Price: £39.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=52">More Info</td></h3>
+
+
+
+	
+
+  </tr>
+  <tr width="200px">
+
+    <td style="padding:20px 20px 20px 20px;"><img src ="./gameimages/24.png" width="200px" height="300px"> </td>
+
+	<td style="padding:20px 20px 20px 20px;"><img src ="./gameimages/27.png" width="200px" height="300px"> </td>
+
+    <td style="padding:20px 20px 20px 20px;"><img src ="./gameimages/32.png" width="200px" height="300px"> </td>
+
+    <td style="padding:20px 20px 20px 20px;"><img src ="./gameimages/35.png" width="200px" height="300px"> </td>
+
+	<td style="padding:20px 20px 20px 20px;"><img src ="./gameimages/37.png" width="200px" height="300px"> </td>
+	 <td style="padding:20px 20px 20px 20px;"><img src ="./gameimages/67.png" width="200px" height="300px"> </td>
+
+    <td style="padding:20px 20px 20px 20px;"><img src ="./gameimages/73.png" width="200px" height="300px"> </td>
+
+    <td style="padding:20px 20px 20px 20px;"><img src ="./gameimages/77.png" width="200px" height="300px"> </td>
+
+    <td style="padding:20px 20px 20px 20px;"><img src ="./gameimages/79.png" width="200px" height="300px"> </td>
+<td style="padding:20px 20px 20px 20px;"><img src ="./gameimages/56.png" width="200px" height="300px"> </td>
+
+    <td style="padding:20px 20px 20px 20px;"><img src ="./gameimages/60.png" width="200px" height="300px"> </td>
+
+  </tr>
+  <tr>
+
+	<td style="padding:0 20px 20px 20px;">Just Dance 2015<br>Price: £30.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=24">More Info</td></h3>
+
+
+
+	<td style="padding:0 20px 20px 20px;">Alien Isolation Nostromo Edition<br>Price: £30.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=27">More Info</td></h3>
+
+
+
+	<td style="padding:0 20px 20px 20px;">Call Of Duty Advanced Warfare Day One Edition<br>Price: £32.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=32">More Info</td></h3>
+
+
+	<td style="padding:0 20px 20px 20px;">Madden 15<br>Price: £32.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=35">More Info</td></h3>
+
+
+
+	<td style="padding:0 20px 20px 20px;">Wolfenstein<br><br>Price: £30.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=37">More Info</td></h3>
+          <td style="padding:0 20px 20px 20px;">Desting Day zero<br>Price: £32.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=67">More Info</td></h3>
+
+
+
+	<td style="padding:0 20px 20px 20px;">Forza 5<br>Price: £39.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=73">More Info</td></h3>
+
+
+
+	<td style="padding:0 20px 20px 20px;">Battlefield 4: Premium<br>Price: £30.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=77">More Info</td></h3>
+
+
+
+	<td style="padding:0 20px 20px 20px;">Call of Duty Ghosts<br>Price: £32.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=79">More Info</td></h3>
+<td style="padding:0 20px 20px 20px;">Middle Earth: Shadow Of Mordor<br>Price: £34.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=56">More Info</td></h3>
+
+
+
+	<td style="padding:0 20px 20px 20px;">Halo MasterCheif<br>Price: £32.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=60">More Info</td></h3>
+  </tr>
+</table> 
+</div>  
+    
+</div>
+<div class="footer">
+
+
+<table>
+</tr>
+<center>
+<td><li><a href="feedbackform.php">Feedback Form</a></li></td>
+
+	<td><li><a href="contactus.php">Contact Us</a></li></td>
+	<td><li><a href="">Product Guarantee </a></li></td>
+	<td><li><a href=""> Twitter </a></li></td>
+	<td><li><a href="">Facebook </a></li></td>
+	<script> 
+document.write(document.lastModified);
+</script>
+</center>
+</tr>
+</table>
+	
+</div>
+        
+
+ </div>   
+</body>
+</html>

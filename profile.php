a708a1dfd707f02fcd92e5e5da1f9f752d14d216
HarleyDobson <harley94@btinternet.com>
2015-03-20 12:12:30 +0000

    Update profile.php

--- profile.php
+++ profile.php
@@ -1,19 +1,19 @@
 <html>
 <head>
 <link href="CSSDesign1.css" rel="stylesheet" type="text/css" />
-<link href="navbar.css" rel="stylesheet" type="text/css" />
+<link href="navbar.css" rel="stylesheet" type="text/css" />  <!-- Link to the CSS pages -->
 
 
-<link rel='shortcut icon' type='image/ico' href='favicon.ico' />
-
-
-
-<?php session_start();?>
+<link rel='shortcut icon' type='image/ico' href='favicon.ico' />  <!-- A favicon is added onto every page -->
 
 
 
+<?php session_start();?>	<!-- The page session is started -->
 
-<Title> Prism Games | Profile</title>
+
+
+
+<Title> Prism Games | Profile</title> <!-- Title for the page is created -->
 
 </head>
 
@@ -21,7 +21,7 @@
 <div class="Pagewrap"><div class="Top-Bar"><div class="Logo"><!--
 --><img src="LOGO3.png" alt="lOGO" style="width:200px; height:150px"><h1 style="font-size: 14px; text-align: center;">PRISM GAMES</h1><!--
 --></div><div class="Hold"><div class="Login">
-<?php include('login-script.php'); ?>
+<?php include('login-script.php'); ?> <!-- Use the login script-->
 </div>
 <div class="Search_Bar">
 <script>
@@ -44,7 +44,7 @@
 <input type="hidden" name="cmd" value="_s-xclick">
 <input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIG1QYJKoZIhvcNAQcEoIIGxjCCBsICAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYC3jjQhaGU3b83dmmcbLQPDYnV8u24jwJ2lOAEXGOGCpQSTIbmtr1scrnC3Ahj7qFYfSyQEse/ZGAHOImb9+2U9NaoJUsDp7KIL+R2JbJL5VGsFAj4lrh/gkXRw59TtokEwFdwWhYAvkpTzm+TuDPCf74lCIbgDHKxk07rI9cJydzELMAkGBSsOAwIaBQAwUwYJKoZIhvcNAQcBMBQGCCqGSIb3DQMHBAg/yIDp6UPu14AwrpmXsneMxaj2Crjoc3WlrRDKFgnDuuqHNpIxuRT0IAiBNyOfo49g7z2DrQT8lDkQoIIDhzCCA4MwggLsoAMCAQICAQAwDQYJKoZIhvcNAQEFBQAwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tMB4XDTA0MDIxMzEwMTMxNVoXDTM1MDIxMzEwMTMxNVowgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDBR07d/ETMS1ycjtkpkvjXZe9k+6CieLuLsPumsJ7QC1odNz3sJiCbs2wC0nLE0uLGaEtXynIgRqIddYCHx88pb5HTXv4SZeuv0Rqq4+axW9PLAAATU8w04qqjaSXgbGLP3NmohqM6bV9kZZwZLR/klDaQGo1u9uDb9lr4Yn+rBQIDAQABo4HuMIHrMB0GA1UdDgQWBBSWn3y7xm8XvVk/UtcKG+wQ1mSUazCBuwYDVR0jBIGzMIGwgBSWn3y7xm8XvVk/UtcKG+wQ1mSUa6GBlKSBkTCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb22CAQAwDAYDVR0TBAUwAwEB/zANBgkqhkiG9w0BAQUFAAOBgQCBXzpWmoBa5e9fo6ujionW1hUhPkOBakTr3YCDjbYfvJEiv/2P+IobhOGJr85+XHhN0v4gUkEDI8r2/rNk1m0GA8HKddvTjyGw/XqXa+LSTlDYkqI8OwR8GEYj4efEtcRpRYBxV8KxAW93YDWzFGvruKnnLbDAF6VR5w/cCMn5hzGCAZowggGWAgEBMIGUMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbQIBADAJBgUrDgMCGgUAoF0wGAYJKoZIhvcNAQkDMQsGCSqGSIb3DQEHATAcBgkqhkiG9w0BCQUxDxcNMTUwMjE5MTczMDA0WjAjBgkqhkiG9w0BCQQxFgQUV/n8SEbkbxc11B3xD5In7Za6NmcwDQYJKoZIhvcNAQEBBQAEgYCFdMdo9TTEjN1qc45PDzH7BHckskrHNwL5byIS4dm9w5DmNQIdYY/v9/BluziMlthhzDPWbn7TGGDr4BM4ykYAUQMwMwiTv3222EH+xLwyFkM0F9ft6078gZUjfsYfoKmybmV1A8AXbIkCFR1GSn77MlF2DpGNTS8WrjEORhXmuw==-----END PKCS7-----
 ">
-<input type="image" src="http://www.computing.northampton.ac.uk/~2027c_1/Cart.png" border="0" name="submit" alt="PayPal – The safer, easier way to pay online." wisth="50" height="50">
+<input type="image" src="http://www.computing.northampton.ac.uk/~2027c_1/Cart.png" border="0" name="submit" alt="PayPal â€“ The safer, easier way to pay online." wisth="50" height="50">
 <img alt="" border="0" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" width="1" height="1">
 </form>
     </div>
@@ -65,18 +65,18 @@
 
 
 <?php 
-$customerid =isset($_GET["customerid"]);
-    include('DB_connection.php');
+$customerid =isset($_GET["customerid"]); <!-- Creating a variable -->
+    include('DB_connection.php');  <!-- Use the script database connection -->
 
-$sql= "select username,emailaddress,delivery_address,delivery_postcode,billing_address,billing_postcode from CUSTOMER WHERE username = '$_SESSION[$Session_name]'";
+$sql= "select username,emailaddress,delivery_address,delivery_postcode,billing_address,billing_postcode from CUSTOMER WHERE username = '$_SESSION[$Session_name]'";  <!--Sql Query -->
 $result=$db ->query($sql);
 if(!$result){
-die('There was an error running the query' .$db->error);
+die('There was an error running the query' .$db->error);  <!-- Error Checking -->
 }
 
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
-$username=$row["username"];
+$username=$row["username"];  <!-- Getting the data from the database -->
 $emailaddress=$row["emailaddress"];
 $delivery_address=$row["delivery_address"];
 $delivery_postcode=$row["delivery_postcode"];
@@ -93,7 +93,7 @@ $billing_postcode=$row["billing_postcode"];
   <tr>
     
     <td width="82" valign="top"><div align="left">Username:</div></td>
-    <td width="165" valign="top"><?php echo ($username) ?></td>
+    <td width="165" valign="top"><?php echo ($username) ?></td>  <!-- Displaying the information from the database -->
   </tr>
   <tr>
     <td valign="top"><div align="left">Email Address:</div></td>

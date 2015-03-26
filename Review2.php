ea3e41ec8a7967e48539eb58d10c21ff589373ee
sabrinanicolepachulski <sabrina.pachulski@btinternet.com>
2015-03-26 18:01:07 +0000

    Update Review2.php

--- Review2.php
+++ Review2.php
@@ -1,15 +1,16 @@
 <html>
 <head>
-<link href="CSSDesign1.css" rel="stylesheet" type="text/css" />
+<link href="CSSDesign1.css" rel="stylesheet" type="text/css" /> <!-- CSS Code is the desgin used and implanted onto the page-->
 <link href="navbar.css" rel="stylesheet" type="text/css" />
-<link rel='shortcut icon' type='image/ico' href='favicon.ico' />
-<?php session_start();?>
-<Title> Prism Games | Homepage</title>
+<link rel='shortcut icon' type='image/ico' href='favicon.ico' /><!--Page wrap uses comments to page sure the logo doesn't move.-->
+<?php session_start();?><!-- php session start login in and starts using php-->
+<Title> Prism Games | Review</title>
 </head>
 <body>
-<div class="Pagewrap"><div class="Top-Bar"><div class="Logo"><!--
+<!--Page wrap uses comments to page sure the logo doesn't move.-->
+<div class="Pagewrap"><div class="Top-Bar"><div class="Logo"><!--Logo placed within the top [ar
 --><img src="LOGO3.png" alt="lOGO" style="width:200px; height:150px"><h1 style="font-size: 14px; text-align: center;">PRISM GAMES</h1><!--
---></div><div class="Hold"><div class="Login">
+login script for users acesss specail privileges  --></div><div class="Hold"><div class="Login">
 <?php include('login-script.php'); ?>
 </div>
 <div class="Search_Bar">
@@ -27,7 +28,7 @@
 </script>
 <gcse:search></gcse:search>
 	</div>
-	<div class="Basket">
+	<div class="Basket"><!--Basket uses paypal and links them to the basket so the user is able to buy prodcuts-->	
 <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post" >
 <input type="hidden" name="cmd" value="_s-xclick">
 <input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIG1QYJKoZIhvcNAQcEoIIGxjCCBsICAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYC3jjQhaGU3b83dmmcbLQPDYnV8u24jwJ2lOAEXGOGCpQSTIbmtr1scrnC3Ahj7qFYfSyQEse/ZGAHOImb9+2U9NaoJUsDp7KIL+R2JbJL5VGsFAj4lrh/gkXRw59TtokEwFdwWhYAvkpTzm+TuDPCf74lCIbgDHKxk07rI9cJydzELMAkGBSsOAwIaBQAwUwYJKoZIhvcNAQcBMBQGCCqGSIb3DQMHBAg/yIDp6UPu14AwrpmXsneMxaj2Crjoc3WlrRDKFgnDuuqHNpIxuRT0IAiBNyOfo49g7z2DrQT8lDkQoIIDhzCCA4MwggLsoAMCAQICAQAwDQYJKoZIhvcNAQEFBQAwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tMB4XDTA0MDIxMzEwMTMxNVoXDTM1MDIxMzEwMTMxNVowgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDBR07d/ETMS1ycjtkpkvjXZe9k+6CieLuLsPumsJ7QC1odNz3sJiCbs2wC0nLE0uLGaEtXynIgRqIddYCHx88pb5HTXv4SZeuv0Rqq4+axW9PLAAATU8w04qqjaSXgbGLP3NmohqM6bV9kZZwZLR/klDaQGo1u9uDb9lr4Yn+rBQIDAQABo4HuMIHrMB0GA1UdDgQWBBSWn3y7xm8XvVk/UtcKG+wQ1mSUazCBuwYDVR0jBIGzMIGwgBSWn3y7xm8XvVk/UtcKG+wQ1mSUa6GBlKSBkTCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb22CAQAwDAYDVR0TBAUwAwEB/zANBgkqhkiG9w0BAQUFAAOBgQCBXzpWmoBa5e9fo6ujionW1hUhPkOBakTr3YCDjbYfvJEiv/2P+IobhOGJr85+XHhN0v4gUkEDI8r2/rNk1m0GA8HKddvTjyGw/XqXa+LSTlDYkqI8OwR8GEYj4efEtcRpRYBxV8KxAW93YDWzFGvruKnnLbDAF6VR5w/cCMn5hzGCAZowggGWAgEBMIGUMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbQIBADAJBgUrDgMCGgUAoF0wGAYJKoZIhvcNAQkDMQsGCSqGSIb3DQEHATAcBgkqhkiG9w0BCQUxDxcNMTUwMjE5MTczMDA0WjAjBgkqhkiG9w0BCQQxFgQUV/n8SEbkbxc11B3xD5In7Za6NmcwDQYJKoZIhvcNAQEBBQAEgYCFdMdo9TTEjN1qc45PDzH7BHckskrHNwL5byIS4dm9w5DmNQIdYY/v9/BluziMlthhzDPWbn7TGGDr4BM4ykYAUQMwMwiTv3222EH+xLwyFkM0F9ft6078gZUjfsYfoKmybmV1A8AXbIkCFR1GSn77MlF2DpGNTS8WrjEORhXmuw==-----END PKCS7-----
@@ -37,6 +38,7 @@
 </form>
 </div></div>
 <div id='nav'>
+<!--Navigation links follows the CSS desgin uses li-->
 <ul>
    <li class='active'><a href='Homepage.php'><span>Home</span></a></li>
    <li><a href='ps4page.php'><span>Ps4</span></a></li>
@@ -46,7 +48,7 @@
 </ul>
 </div></div>
 <div class="Main-Content">
-
+<!--table used to display images within a table so that it follows the desgin and also links moves with a scrollbar-->
 </br>
 
 
@@ -55,7 +57,7 @@
 
 
 <iframe width="700" height="480" align="right" src="https://www.youtube.com/embed/ei0DKYdlTK0" frameborder="0" allowfullscreen></iframe></p>
-
+<!--Iframe is used-->	
 
 
 <p align="right center" style="
@@ -65,40 +67,33 @@
   
 ">
 
-The Last of Us is an action-adventure survival horror video game developed by Naughty Dog and published by Sony Computer Entertainment. It was released for the PlayStation 3 on June 14, 2013; an updated version, The Last of Us Remastered, was released for the PlayStation 4 on July 29, 2014. The player controls Joel, a man tasked with escorting the young Ellie across a post-apocalyptic United States.
 
-<p>The Last of Us is played from a third-person perspective; players use firearms, improvised weapons and stealth to defend against hostile humans and zombie-like creatures infected by a mutated strain of the Cordyceps fungus. "Listen mode" lets players locate enemies through a heightened sense of hearing and spatial awareness. Players can also customize weapons and items using items scavenged from the environment. In the game's online multiplayer, up to eight players engage in co-operative and competitive gameplay in recreations of single-player settings.
-</p>
+Exclusive to Xbox One
+Less a compilation, more a celebration, Halo: The Master Chief Collection runs in danger of giving you too much of a good thing. We all know Bungie’s Halo mantra – thirty seconds of fun that happens over and over again – but here we get that thirty seconds repeated over some forty hours of epic sci-fi action, and that’s without considering multiplayer, co-op and replay value. Do you really love Halo that much?
 
-The Last of Us Remastered is released for PS4
-its amazing
-
+Perhaps you ought to, because The Master Chief Collection proves that Bungie’s series can last the distance. The first three Halo games have all dated to some extent, and playing them again throws up flaws you either didn’t spot first time around, or that your brain has generously glossed over. Yet it’s still easy to see why each game in this collection is considered a classic. All hold up remarkably well today. 
+Read more at 
 
 something else here
 <p><h1> Graphics rating </h1></p>
-<p><img  style="" src ="./reviewimages/One.png" width="400px" height="150px"></p>
+<p><img  style="" src ="./reviewimages/2.png" width="400px" height="150px"></p>
 
 <p><h1> Gaming rating </h1></p>
-<p><img  style="" src ="./reviewimages/R2.jpg" width="400px" height="150px"></p>
+<p><img  style="" src ="./reviewimages/2.png" width="400px" height="150px"></p>
  
 <p><h1> Overall rating </h1></p>
-<p><img  style="" src ="./reviewimages/R3.jpg" width="400px" height="150px">
+<p><img  style="" src ="./reviewimages/2.png" width="400px" height="150px">
  
 
 
-<CHANGE THIS TO THE TRANS>
+
 
 </br>
 </br>
 </form>
 </div>
 
- 
-
-
-    
-
-       
+<!--table used to place links for the footer in a stairght line-->
 <div class="footer">
 <table>
 <tr>
@@ -110,9 +105,7 @@ something else here
 	<td><ul><a href="GameReview.php">Game Reviews </a></ul></td>
 	<td><ul><a href="https://twitter.com/Prism_Games_"> Twitter </a></ul></td>
 	<td><ul><a href="">Facebook </a></ul></td>
-	<script> 
-document.write(document.lastModified);
-</script>
+
 </center>
 </tr>
 </table>

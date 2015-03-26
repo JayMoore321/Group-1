ab7dcca98387451d1ab04724878ac7ea0cc21dee
sabrinanicolepachulski <sabrina.pachulski@btinternet.com>
2015-03-26 18:08:04 +0000

    Update Review4.php

--- Review4.php
+++ Review4.php
@@ -1,15 +1,16 @@
 <html>
 <head>
-<link href="CSSDesign1.css" rel="stylesheet" type="text/css" />
+<link href="CSSDesign1.css" rel="stylesheet" type="text/css" /> <!-- CSS Code is the desgin used and implanted onto the page-->
 <link href="navbar.css" rel="stylesheet" type="text/css" />
-<link rel='shortcut icon' type='image/ico' href='favicon.ico' />
-<?php session_start();?>
-<Title> Prism Games | Homepage</title>
+<link rel='shortcut icon' type='image/ico' href='favicon.ico' /><!--Page wrap uses comments to page sure the logo doesn't move.-->
+<?php session_start();?><!-- php session start login in and starts using php-->
+<Title> Prism Games | Review</title>
 </head>
 <body>
-<div class="Pagewrap"><div class="Top-Bar"><div class="Logo"><!--
+<!--Page wrap uses comments to page sure the logo doesn't move.-->
+<div class="Pagewrap"><div class="Top-Bar"><div class="Logo"><!--Logo placed within the top [ar
 --><img src="LOGO3.png" alt="lOGO" style="width:200px; height:150px"><h1 style="font-size: 14px; text-align: center;">PRISM GAMES</h1><!--
---></div><div class="Hold"><div class="Login">
+login script for users acesss specail privileges  --></div><div class="Hold"><div class="Login">
 <?php include('login-script.php'); ?>
 </div>
 <div class="Search_Bar">
@@ -27,7 +28,7 @@
 </script>
 <gcse:search></gcse:search>
 	</div>
-	<div class="Basket">
+	<div class="Basket"><!--Basket uses paypal and links them to the basket so the user is able to buy prodcuts-->	
 <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post" >
 <input type="hidden" name="cmd" value="_s-xclick">
 <input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIG1QYJKoZIhvcNAQcEoIIGxjCCBsICAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYC3jjQhaGU3b83dmmcbLQPDYnV8u24jwJ2lOAEXGOGCpQSTIbmtr1scrnC3Ahj7qFYfSyQEse/ZGAHOImb9+2U9NaoJUsDp7KIL+R2JbJL5VGsFAj4lrh/gkXRw59TtokEwFdwWhYAvkpTzm+TuDPCf74lCIbgDHKxk07rI9cJydzELMAkGBSsOAwIaBQAwUwYJKoZIhvcNAQcBMBQGCCqGSIb3DQMHBAg/yIDp6UPu14AwrpmXsneMxaj2Crjoc3WlrRDKFgnDuuqHNpIxuRT0IAiBNyOfo49g7z2DrQT8lDkQoIIDhzCCA4MwggLsoAMCAQICAQAwDQYJKoZIhvcNAQEFBQAwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tMB4XDTA0MDIxMzEwMTMxNVoXDTM1MDIxMzEwMTMxNVowgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDBR07d/ETMS1ycjtkpkvjXZe9k+6CieLuLsPumsJ7QC1odNz3sJiCbs2wC0nLE0uLGaEtXynIgRqIddYCHx88pb5HTXv4SZeuv0Rqq4+axW9PLAAATU8w04qqjaSXgbGLP3NmohqM6bV9kZZwZLR/klDaQGo1u9uDb9lr4Yn+rBQIDAQABo4HuMIHrMB0GA1UdDgQWBBSWn3y7xm8XvVk/UtcKG+wQ1mSUazCBuwYDVR0jBIGzMIGwgBSWn3y7xm8XvVk/UtcKG+wQ1mSUa6GBlKSBkTCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb22CAQAwDAYDVR0TBAUwAwEB/zANBgkqhkiG9w0BAQUFAAOBgQCBXzpWmoBa5e9fo6ujionW1hUhPkOBakTr3YCDjbYfvJEiv/2P+IobhOGJr85+XHhN0v4gUkEDI8r2/rNk1m0GA8HKddvTjyGw/XqXa+LSTlDYkqI8OwR8GEYj4efEtcRpRYBxV8KxAW93YDWzFGvruKnnLbDAF6VR5w/cCMn5hzGCAZowggGWAgEBMIGUMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbQIBADAJBgUrDgMCGgUAoF0wGAYJKoZIhvcNAQkDMQsGCSqGSIb3DQEHATAcBgkqhkiG9w0BCQUxDxcNMTUwMjE5MTczMDA0WjAjBgkqhkiG9w0BCQQxFgQUV/n8SEbkbxc11B3xD5In7Za6NmcwDQYJKoZIhvcNAQEBBQAEgYCFdMdo9TTEjN1qc45PDzH7BHckskrHNwL5byIS4dm9w5DmNQIdYY/v9/BluziMlthhzDPWbn7TGGDr4BM4ykYAUQMwMwiTv3222EH+xLwyFkM0F9ft6078gZUjfsYfoKmybmV1A8AXbIkCFR1GSn77MlF2DpGNTS8WrjEORhXmuw==-----END PKCS7-----
@@ -36,7 +37,8 @@
 <img alt="" border="0" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" width="1" height="1">
 </form>
 </div></div>
-<div id='nav'>
+<div id='nav'></div> 
+<!--Navigation links follows the CSS desgin uses li-->
 <ul>
    <li class='active'><a href='Homepage.php'><span>Home</span></a></li>
    <li><a href='ps4page.php'><span>Ps4</span></a></li>
@@ -46,6 +48,8 @@
 </ul>
 </div></div>
 <div class="Main-Content">
+<!--table used to display images within a table so that it follows the desgin and also links moves with a scrollbar-->
+
 
 </br>
 
@@ -65,13 +69,15 @@
   
 ">
 
-The Last of Us is an action-adventure survival horror video game developed by Naughty Dog and published by Sony Computer Entertainment. It was released for the PlayStation 3 on June 14, 2013; an updated version, The Last of Us Remastered, was released for the PlayStation 4 on July 29, 2014. The player controls Joel, a man tasked with escorting the young Ellie across a post-apocalyptic United States.
+Grand Theft Auto 5 releases on Xbox One and PlayStation 4 today, bringing Rockstar's expansive open world game to the new generation of consoles. Digital Foundry will be assessing the technical differences throughout the week, and we'll be bringing you more on what's new and how the original stands up nearly a year on from its release. To start, though, here's our original review, first published in September 2013.
 
-<p>The Last of Us is played from a third-person perspective; players use firearms, improvised weapons and stealth to defend against hostile humans and zombie-like creatures infected by a mutated strain of the Cordyceps fungus. "Listen mode" lets players locate enemies through a heightened sense of hearing and spatial awareness. Players can also customize weapons and items using items scavenged from the environment. In the game's online multiplayer, up to eight players engage in co-operative and competitive gameplay in recreations of single-player settings.
-</p>
+If there was one criticism of Grand Theft Auto 4 that evidently stung Rockstar, it was the complaint that it lacked an endgame. Liberty City was an incredible place, cramming as much character into one city block as most open worlds manage in a thousand, but once Niko settled his last score, there wasn't much to do but cruise around waiting for the DLC.
 
-The Last of Us Remastered is released for PS4
-its amazing
+Grand Theft Auto 5 is a welcome overreaction. Rockstar has rammed Los Santos and the surrounding desert and mountain areas with more things to do than I could describe in half a dozen reviews. I'm not sure it feels like the biggest open world in the series' history, but I think that's just because it's so easy to travel across quickly, and it's certainly the most densely packed with hedonistic thrills, stuff to buy and steal, random events and weirdoes who want something. Then there's the promise of GTA Online, the evolving, persistent multiplayer component due to land for free at the start of October.
+
+Packing in all those activities - from trash-diving to skydiving - hasn't impeded Rockstar's world-building either. Los Santos takes the basic geography of Los Angeles and files it down into something tight and entertaining to navigate, where every street has its own story etched in phony colonnades or chain-link fences and landmarks are lifted from real life (Grauman's Chinese, Chateau Marmont) or the silver screen (the house on stilts in Lethal Weapon 2 springs to mind), then woven together with practised ease.
+
+
 
 
 something else here

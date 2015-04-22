7329b8bdcbbe23db3bf7f963e4830c8ead2ab87f
sabrinanicolepachulski <sabrina.pachulski@btinternet.com>
2015-04-22 12:09:48 +0100

    Update DB_connection.php

--- DB_connection.php
+++ DB_connection.php
@@ -1,10 +1,10 @@
 <?php
-    DEFINE ('DB_HOST', '194.81.104.22');
-	DEFINE ('DB_USER', 'team1');
-	DEFINE ('DB_PASSWORD', 'group1');
-	DEFINE ('DB_NAME', 'db_team1');
+    DEFINE ('DB_HOST', '194.81.104.22');  // Host IP Address 
+	DEFINE ('DB_USER', 'team1'); //User Name to long into 
+	DEFINE ('DB_PASSWORD', 'group1'); //Password to allow log in
+	DEFINE ('DB_NAME', 'db_team1');  //Database name
 
-	$db = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
-	OR die('Could not connect to MySQL: ' .
-	mysqli_connect_error());
+	$db = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) //MYSQLI is used to create the connectiom
+	OR die('Could not connect to MySQL: ' . // if connection could not happen
+	mysqli_connect_error()); // Any errors that occur 
 	?>

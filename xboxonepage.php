ca4f3653748dc41ae2e0b04463fbd7ee60ee6bfb
sabrinanicolepachulski <sabrina.pachulski@btinternet.com>
2015-04-22 11:44:20 +0100

    Update xboxonepage.php

--- xboxonepage.php
+++ xboxonepage.php
@@ -39,7 +39,7 @@
 <input type="hidden" name="cmd" value="_s-xclick">
 <input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIG1QYJKoZIhvcNAQcEoIIGxjCCBsICAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYC3jjQhaGU3b83dmmcbLQPDYnV8u24jwJ2lOAEXGOGCpQSTIbmtr1scrnC3Ahj7qFYfSyQEse/ZGAHOImb9+2U9NaoJUsDp7KIL+R2JbJL5VGsFAj4lrh/gkXRw59TtokEwFdwWhYAvkpTzm+TuDPCf74lCIbgDHKxk07rI9cJydzELMAkGBSsOAwIaBQAwUwYJKoZIhvcNAQcBMBQGCCqGSIb3DQMHBAg/yIDp6UPu14AwrpmXsneMxaj2Crjoc3WlrRDKFgnDuuqHNpIxuRT0IAiBNyOfo49g7z2DrQT8lDkQoIIDhzCCA4MwggLsoAMCAQICAQAwDQYJKoZIhvcNAQEFBQAwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tMB4XDTA0MDIxMzEwMTMxNVoXDTM1MDIxMzEwMTMxNVowgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDBR07d/ETMS1ycjtkpkvjXZe9k+6CieLuLsPumsJ7QC1odNz3sJiCbs2wC0nLE0uLGaEtXynIgRqIddYCHx88pb5HTXv4SZeuv0Rqq4+axW9PLAAATU8w04qqjaSXgbGLP3NmohqM6bV9kZZwZLR/klDaQGo1u9uDb9lr4Yn+rBQIDAQABo4HuMIHrMB0GA1UdDgQWBBSWn3y7xm8XvVk/UtcKG+wQ1mSUazCBuwYDVR0jBIGzMIGwgBSWn3y7xm8XvVk/UtcKG+wQ1mSUa6GBlKSBkTCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb22CAQAwDAYDVR0TBAUwAwEB/zANBgkqhkiG9w0BAQUFAAOBgQCBXzpWmoBa5e9fo6ujionW1hUhPkOBakTr3YCDjbYfvJEiv/2P+IobhOGJr85+XHhN0v4gUkEDI8r2/rNk1m0GA8HKddvTjyGw/XqXa+LSTlDYkqI8OwR8GEYj4efEtcRpRYBxV8KxAW93YDWzFGvruKnnLbDAF6VR5w/cCMn5hzGCAZowggGWAgEBMIGUMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbQIBADAJBgUrDgMCGgUAoF0wGAYJKoZIhvcNAQkDMQsGCSqGSIb3DQEHATAcBgkqhkiG9w0BCQUxDxcNMTUwMjE5MTczMDA0WjAjBgkqhkiG9w0BCQQxFgQUV/n8SEbkbxc11B3xD5In7Za6NmcwDQYJKoZIhvcNAQEBBQAEgYCFdMdo9TTEjN1qc45PDzH7BHckskrHNwL5byIS4dm9w5DmNQIdYY/v9/BluziMlthhzDPWbn7TGGDr4BM4ykYAUQMwMwiTv3222EH+xLwyFkM0F9ft6078gZUjfsYfoKmybmV1A8AXbIkCFR1GSn77MlF2DpGNTS8WrjEORhXmuw==-----END PKCS7-----
 ">
-<input type="image" src="http://www.computing.northampton.ac.uk/~2027c_1/Cart.png" border="0" name="submit" alt="PayPal – The safer, easier way to pay online." wisth="50" height="50">
+<input type="image" src="http://www.computing.northampton.ac.uk/~2027c_1/Cart.png" border="0" name="submit" alt="PayPal â€“ The safer, easier way to pay online." wisth="50" height="50">
 <img alt="" border="0" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" width="1" height="1">
 </form>
     </div>
@@ -88,45 +88,45 @@
 	<td style="padding:20px 20px 20px 20px;"><img src ="./gameimages/61.png" width="200px" height="300px"> </td>
   </tr>
   <tr>
-    <td style="padding:0 20px 20px 20px;">Tomb Raider Definitive Edition<br>Price: £42.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=42">More Info</td></h3>
+    <td style="padding:0 20px 20px 20px;">Tomb Raider Definitive Edition<br>Price: Â£42.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=42">More Info</td></h3>
 
-	<td style="padding:0 20px 20px 20px;">Saints Row IV<br>Price: £30.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=43">More Info</td></h3>
+	<td style="padding:0 20px 20px 20px;">Saints Row IV<br>Price: Â£30.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=43">More Info</td></h3>
 
-	<td style="padding:0 20px 20px 20px;">Call of Duty: Advanced Warfare<br>Price: £42.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=44">More Info</td></h3>
+	<td style="padding:0 20px 20px 20px;">Call of Duty: Advanced Warfare<br>Price: Â£42.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=44">More Info</td></h3>
 
-	<td style="padding:0 20px 20px 20px;">Fifa 15<br>Price: £32.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=45">More Info</td></h3>
+	<td style="padding:0 20px 20px 20px;">Fifa 15<br>Price: Â£32.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=45">More Info</td></h3>
 
-	<td style="padding:0 20px 20px 20px;">Minecraft<br>Price: £14.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=46">More Info</td></h3>
+	<td style="padding:0 20px 20px 20px;">Minecraft<br>Price: Â£14.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=46">More Info</td></h3>
 
-	<td style="padding:0 20px 20px 20px;">Far Cry 4 Limited Edition<br>Price: £42.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=47">More Info</td></h3>
+	<td style="padding:0 20px 20px 20px;">Far Cry 4 Limited Edition<br>Price: Â£42.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=47">More Info</td></h3>
 
-	<td style="padding:0 20px 20px 20px;">Destiny<br>Price: £42.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=48">More Info</td></h3>
+	<td style="padding:0 20px 20px 20px;">Destiny<br>Price: Â£42.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=48">More Info</td></h3>
 
-	<td style="padding:0 20px 20px 20px;">Forza Horison 2<br>Price: £34.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=49">More Info</td></h3>
+	<td style="padding:0 20px 20px 20px;">Forza Horison 2<br>Price: Â£34.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=49">More Info</td></h3>
 
-	<td style="padding:0 20px 20px 20px;">The Crew<br>Price: £32.00 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=50">More Info</td></h3>
+	<td style="padding:0 20px 20px 20px;">The Crew<br>Price: Â£32.00 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=50">More Info</td></h3>
 
-	<td style="padding:0 20px 20px 20px;">Halo Master cheif collection<br>Price: £29.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=51">More Info</td></h3>
+	<td style="padding:0 20px 20px 20px;">Halo Master cheif collection<br>Price: Â£29.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=51">More Info</td></h3>
 
-	<td style="padding:0 20px 20px 20px;">Rugny 15<br>Price: £39.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=52">More Info</td></h3>
+	<td style="padding:0 20px 20px 20px;">Rugny 15<br>Price: Â£39.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=52">More Info</td></h3>
 
-	<td style="padding:0 20px 20px 20px;">Sunset Overdrive<br>Price: £40.00 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=53">More Info</td></h3>
+	<td style="padding:0 20px 20px 20px;">Sunset Overdrive<br>Price: Â£40.00 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=53">More Info</td></h3>
 
-	<td style="padding:0 20px 20px 20px;">Dragon Age: Inquisition<br>Price: £44.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=54">More Info</td></h3>
+	<td style="padding:0 20px 20px 20px;">Dragon Age: Inquisition<br>Price: Â£44.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=54">More Info</td></h3>
 
-	<td style="padding:0 20px 20px 20px;">W2K15<br>Price: £29.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=55">More Info</td></h3>
+	<td style="padding:0 20px 20px 20px;">W2K15<br>Price: Â£29.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=55">More Info</td></h3>
 
-	<td style="padding:0 20px 20px 20px;">Middle Earth: Shadow Of Mordor<br>Price: £34.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=56">More Info</td></h3>
+	<td style="padding:0 20px 20px 20px;">Middle Earth: Shadow Of Mordor<br>Price: Â£34.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=56">More Info</td></h3>
 
-	<td style="padding:0 20px 20px 20px;">Alien: Isolation Ripley<br>Price: £28.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=57">More Info</td></h3>
+	<td style="padding:0 20px 20px 20px;">Alien: Isolation Ripley<br>Price: Â£28.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=57">More Info</td></h3>
 
-	<td style="padding:0 20px 20px 20px;">Lego Marvel superheros<br>Price: £17.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=58">More Info</td></h3>
+	<td style="padding:0 20px 20px 20px;">Lego Marvel superheros<br>Price: Â£17.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=58">More Info</td></h3>
 
-	<td style="padding:0 20px 20px 20px;">Battlefield 4<br>Price: £15.98 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=59">More Info</td></h3>
+	<td style="padding:0 20px 20px 20px;">Battlefield 4<br>Price: Â£15.98 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=59">More Info</td></h3>
 
-	<td style="padding:0 20px 20px 20px;">Halo MasterCheif<br>Price: £32.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=60">More Info</td></h3>
+	<td style="padding:0 20px 20px 20px;">Halo MasterCheif<br>Price: Â£32.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=60">More Info</td></h3>
 
-	<td style="padding:0 20px 20px 20px;">Sleeping Dogs<br>Price: £22.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=61">More Info</td></h3>
+	<td style="padding:0 20px 20px 20px;">Sleeping Dogs<br>Price: Â£22.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=61">More Info</td></h3>
 
   </tr>
   <tr width="200px">
@@ -152,44 +152,44 @@
 	<td style="padding:20px 20px 20px 20px;"><img src ="./gameimages/81.png" width="200px" height="300px"> </td>
   </tr>
   <tr>
-    <td style="padding:0 20px 20px 20px;">Lords of the Fallen<br>Price: £28.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=62">More Info</td></h3>
-	<td style="padding:0 20px 20px 20px;">Watch Dogs<br>Price: £26.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=63">More Info</td></h3>
+    <td style="padding:0 20px 20px 20px;">Lords of the Fallen<br>Price: Â£28.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=62">More Info</td></h3>
+	<td style="padding:0 20px 20px 20px;">Watch Dogs<br>Price: Â£26.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=63">More Info</td></h3>
 
-	<td style="padding:0 20px 20px 20px;">Alien Isolation Nostromo<br>Price: £19.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=64">More Info</td></h3>
+	<td style="padding:0 20px 20px 20px;">Alien Isolation Nostromo<br>Price: Â£19.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=64">More Info</td></h3>
 
-	<td style="padding:0 20px 20px 20px;">Trails Fusion<br>Price: £22.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=65">More Info</td></h3>
+	<td style="padding:0 20px 20px 20px;">Trails Fusion<br>Price: Â£22.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=65">More Info</td></h3>
 
-	<td style="padding:0 20px 20px 20px;">Thief<br>Price: £20.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=66">More Info</td></h3>
+	<td style="padding:0 20px 20px 20px;">Thief<br>Price: Â£20.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=66">More Info</td></h3>
 
-	<td style="padding:0 20px 20px 20px;">Desting Day zero<br>Price: £32.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=67">More Info</td></h3>
+	<td style="padding:0 20px 20px 20px;">Desting Day zero<br>Price: Â£32.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=67">More Info</td></h3>
 
-	<td style="padding:0 20px 20px 20px;">Fighter Within<br>Price: £10.11 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=68">More Info</td></h3>
+	<td style="padding:0 20px 20px 20px;">Fighter Within<br>Price: Â£10.11 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=68">More Info</td></h3>
 
-	<td style="padding:0 20px 20px 20px;">Titanfall<br>Price: £22.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=69">More Info</td></h3>
+	<td style="padding:0 20px 20px 20px;">Titanfall<br>Price: Â£22.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=69">More Info</td></h3>
 
-	<td style="padding:0 20px 20px 20px;">UFC<br>Price: £22.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=70">More Info</td></h3>
+	<td style="padding:0 20px 20px 20px;">UFC<br>Price: Â£22.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=70">More Info</td></h3>
 
-	<td style="padding:0 20px 20px 20px;">Wolfenstein Occupied Edition<br>Price: £19.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=71">More Info</td></h3>
+	<td style="padding:0 20px 20px 20px;">Wolfenstein Occupied Edition<br>Price: Â£19.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=71">More Info</td></h3>
 
-	<td style="padding:0 20px 20px 20px;">Ryse: Son of Rome<br>Price: £44.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=72">More Info</td></h3>
+	<td style="padding:0 20px 20px 20px;">Ryse: Son of Rome<br>Price: Â£44.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=72">More Info</td></h3>
 
-	<td style="padding:0 20px 20px 20px;">Forza 5<br>Price: £39.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=73">More Info</td></h3>
+	<td style="padding:0 20px 20px 20px;">Forza 5<br>Price: Â£39.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=73">More Info</td></h3>
 
-	<td style="padding:0 20px 20px 20px;">Sniper Elite 3<br>Price: £26.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=74">More Info</td></h3>
+	<td style="padding:0 20px 20px 20px;">Sniper Elite 3<br>Price: Â£26.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=74">More Info</td></h3>
 
-	<td style="padding:0 20px 20px 20px;">Plants vs Zombies<br>Price: £21.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=75">More Info</td></h3>
+	<td style="padding:0 20px 20px 20px;">Plants vs Zombies<br>Price: Â£21.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=75">More Info</td></h3>
 
-	<td style="padding:0 20px 20px 20px;">Fifa 14<br>Price: £18.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=76">More Info</td></h3>
+	<td style="padding:0 20px 20px 20px;">Fifa 14<br>Price: Â£18.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=76">More Info</td></h3>
 
-	<td style="padding:0 20px 20px 20px;">Battlefield 4: Premium<br>Price: £30.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=77">More Info</td></h3>
+	<td style="padding:0 20px 20px 20px;">Battlefield 4: Premium<br>Price: Â£30.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=77">More Info</td></h3>
 
-	<td style="padding:0 20px 20px 20px;">NBA 2K15<br>Price: £44.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=78">More Info</td></h3>
+	<td style="padding:0 20px 20px 20px;">NBA 2K15<br>Price: Â£44.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=78">More Info</td></h3>
 
-	<td style="padding:0 20px 20px 20px;">Call of Duty Ghosts<br>Price: £32.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=79">More Info</td></h3>
+	<td style="padding:0 20px 20px 20px;">Call of Duty Ghosts<br>Price: Â£32.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=79">More Info</td></h3>
 
-	<td style="padding:0 20px 20px 20px;">The Amazing Spiderman 2<br>Price: £20.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=80">More Info</td></h3>
+	<td style="padding:0 20px 20px 20px;">The Amazing Spiderman 2<br>Price: Â£20.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=80">More Info</td></h3>
 
-	<td style="padding:0 20px 20px 20px;">Dying Light<br>Price: £44.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=81">More Info</td></h3>
+	<td style="padding:0 20px 20px 20px;">Dying Light<br>Price: Â£44.99 <br>  <h3><a  href="http://www.computing.northampton.ac.uk/~2027c_1/ProductPage.php?gameid=81">More Info</td></h3>
 
   </tr>
 </table> 
@@ -197,31 +197,20 @@
     
 
     
-<div class="footer">
-
-
-<table>
+<div class="footer"><!--footer div started-->
+<table> <!--table used to place links for the footer in a stairght line-->
 </tr>
 <center>
 <td><li><a href="feedbackform.php">Feedback Form</a></li></td>
-
-	<td><li><a href="contactus.php">Contact Us</a></li></td>
-	<td><li><a href="productguarantee.php">Product Guarantee </a></li></td>
-	<td><li><a href=""> Twitter </a></li></td>
-	<td><li><a href="">Facebook </a></li></td>
-	<script> 
-document.write(document.lastModified);
-</script>
+<td><li><a href="contactus.php">Contact Us</a></li></td>
+<td><li><a href="productguarantee.php">Product Guarantee </a></li></td>
+<td><li><a href="GameReview.php">Game Reviews </a></li></td>
+<td><li><a href=""> Twitter </a></li></td>
+<td><li><a href="">Facebook </a></li></td>
 </center>
 </tr>
 </table>
-	
 </div>
-  
-    
-    
-    
 </body>    
-  
 </div>
 </html>
